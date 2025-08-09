c9767e9b9c13ea0a94a729720a4ffde45b33ae94
johanstevem <106841615+johanstevem@users.noreply.github.com>
2025-08-09 08:39:31 -0500

    cambios actuales en el js

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//creamos el array donde estaran los amigos guardados
+let amigos=[];
+
+//creamos el elemento aqui con el boton
+
+
+//agarramos lo que el usuario ingrese en la interfaz
+const boton=document.querySelector(".button-add");
+
+boton.addEventListener("click", function(){
+    alert("funciona")
+})
